02c00ba02f12e86aacd54ce30ed7d1c46a68592e
Allison Lucas <allisonlucas@Allisons-MacBook-Pro-2.local>
2019-01-06 11:32:45 -0700

    penlink code challenge complete

--- src/App.js
+++ src/App.js
@@ -1,24 +1,46 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import './app.css';
+import { formatType, sortData } from './utils/dataFormatting';
+import data from './data.json';
 
 class App extends Component {
+  state = {
+    data
+  }
+
+  _getList = () => {
+    const sortedClone = sortData(data);
+    // map over the sorted array and display as list items
+    return sortedClone.map((d, i) => (
+        <li key={d.id}>
+          <div className={'titleContainer'}>
+            <h3>{d.title}</h3>
+            <span className={'type'}>
+              {formatType(d.type)}
+            </span>
+          </div>
+          <div>
+            <button onClick={() => {
+              this.setState({
+                data: data.splice(i, 1)
+              })
+            }}>
+              REMOVE
+            </button>
+          </div>
+        </li>
+      )
+    );
+  }
+
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
+      <div className="app">
+        <header className="app-header">
+          <h2>
+            PenLink Coding Challenge
+          </h2>
+          <ul>{this._getList()}</ul>
         </header>
       </div>
     );
--- src/App.test.js
+++ src/App.test.js
@@ -1,9 +1,37 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
+const dataFormatting = require('./utils/dataFormatting');
 
-it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
-  ReactDOM.unmountComponentAtNode(div);
+const dataToSort = [
+    {
+        "title": "c"
+    },
+    {
+        "title": "a"
+    },
+    {
+        "title": "b"
+    }
+];
+const sortedData = [
+    {
+        "title": "a"
+    },
+    {
+        "title": "b"
+    },
+    {
+        "title": "c"
+    }
+];
+
+test('formats data type', () => {
+    expect(dataFormatting.formatType('phone_number and cell-number'))
+        .toBe('Phone Number And Cell Number');
+});
+
+test('sorts data alphabetically by title', () => {
+    expect(dataFormatting.sortData(dataToSort)).toEqual(sortedData)
+});
+
+test('sorting does not alter original data array', () => {
+    expect(dataFormatting.sortData(dataToSort)).not.toEqual(dataToSort)
 });
